174383f439d6aa1529aea615c7113b26bfeeb338
Ryan Whitten <ryan.whitten@bestegg.com>
2020-12-10 16:21:08 -0500

    Add fixed width file tools

---
+++ data_utils/file_interfaces/fixed_width/data_type_parser.py
@@ -0,0 +1,162 @@
+import logging
+import re
+from datetime import datetime
+from typing import Union
+
+import pendulum
+
+from data_utils.file_interfaces.fixed_width.file_spec import FileSpec
+
+logger = logging.getLogger(__name__)
+
+
+class DataTypeParser:
+    def __init__(self, file_spec: FileSpec):
+        self.spec = file_spec
+
+    def cast_data_types(self, value: str, data_type: str, pic_clause: str, field_name: str) -> Union[str, int]:
+        """Best effort try to cast data types to the types specified in the layout spec."""
+        if data_type == "N" and "." not in pic_clause and "V" not in pic_clause:
+            try:
+                value = int(value.replace("{", ""))
+            except ValueError:
+                logger.info(f"{field_name} value with type N cannot be cast to int: {value}")
+        elif data_type == "N" and ("." in pic_clause or "V" in pic_clause):
+            value = self._parse_decimal_field(value, field_name, pic_clause)
+        elif data_type == "D":
+            value = self._parse_date_field(value, field_name, pic_clause)
+        elif data_type == "T":
+            value = self._parse_time_field(value)
+
+        # Cast empty strings to null
+        value = None if value == "" else value
+
+        return value
+
+    @staticmethod
+    def _parse_decimal_field(value: str, field_name: str, pic_clause: str) -> Union[str, None]:
+        """Uses decimal-based pic_clause to try and convert string to float."""
+        parts = re.findall(r'\((.*?) *\)', pic_clause)
+        try:
+            value = value.replace("{", "")
+            if "." in value:
+                value = float(value)
+            else:
+                value = float(f"{value[:int(parts[0])]}.{value[int(parts[0]):]}")
+        except ValueError:
+            logger.info(
+                f"{field_name} value with type N and decimal pic cannot be cast to float: {value}")
+
+        return value
+
+    def _parse_date_field(self, value: str, field_name: str, pic_clause: str) -> Union[str, None]:
+        """Tries to parse a myriad of inconsistent TSYS date formats to a consistent ISO8601 date string."""
+        if len(value) == 8:  # e.g. 20200813
+            try:
+                value = self._parse_date_string(value)
+            except ValueError:
+                logger.info(f"{field_name} value with type D cannot be converted to date: {value}")
+                value = None
+        elif len(value) == 7:  # e.g. 2020153 julian date
+            try:
+                value = datetime.strptime(value, '%Y%j').date().strftime('%Y-%m-%d')
+            except ValueError:
+                logger.info(f"{field_name} value with type D cannot be converted to date: {value}")
+                value = None
+        elif len(value) == 6 or len(value) == 5:  # e.g. 082023
+            try:
+                date_format = self.spec.cast_fields[field_name].get("date_format") if \
+                    field_name in self.spec.cast_fields else None
+                value = self._parse_expiration_date(value, date_format)
+            except ValueError:
+                logger.info(f"{field_name} value with type D cannot be converted to date: {value}")
+                value = None
+        elif len(value) == 4:  # e.g. 0153 last digit of year + julian day
+            date_format = self.spec.cast_fields[field_name].get("date_format") if \
+                field_name in self.spec.cast_fields else None
+
+            if date_format and date_format in {"%y%m", "%m%y"}:
+                try:
+                    exp_date = datetime.strptime(value, date_format)
+                    return self._parse_expiration_date(exp_date.strftime("%m%Y"))
+                except ValueError:
+                    logger.info(f"{field_name} value with type D cannot be converted to date: {value}")
+                    value = None
+                    return value
+
+            try:
+                year_last_digit = value[0]
+                julian_date = value[1:]
+                year_prefix = str(datetime.now().date().year)[:-1]
+                full_julian_date = year_prefix + year_last_digit + julian_date
+                value = datetime.strptime(full_julian_date, '%Y%j').date().strftime('%Y-%m-%d')
+            except ValueError:
+                logger.info(f"{field_name} value with type D cannot be converted to date: {value}")
+                value = None
+        elif len(value) == 3 and pic_clause == "9(6)":
+            # Special case where dates are listed as 9(6), are really YYMM, but arrive as 3 digits: 923 instead of 0923
+            value = "0" + value
+            date_format = self.spec.cast_fields[field_name].get("date_format") if \
+                field_name in self.spec.cast_fields else None
+
+            if date_format and date_format in {"%y%m", "%m%y"}:
+                exp_date = datetime.strptime(value, date_format)
+                try:
+                    return self._parse_expiration_date(exp_date.strftime("%m%Y"))
+                except ValueError:
+                    logger.info(
+                        f"{field_name} value with type D cannot be converted to date: {value}")
+                    value = None
+                    return value
+
+        return value
+
+    @staticmethod
+    def _parse_date_string(date: str) -> str:
+        """Parse standard date strings, usually 8 char"""
+        for fmt in ['%Y%m%d', '%m%d%Y', '%m/%d/%y']:
+            try:
+                return datetime.strptime(date, fmt).date().strftime('%Y-%m-%d')
+            except ValueError:
+                pass
+
+        raise ValueError
+
+    @staticmethod
+    def _parse_time_field(value: str) -> str:
+        """Parse 6 or 7 char time strings. 7 char times end in 0, which we strip to parse."""
+        if len(value) not in {6, 7}:
+            return value
+
+        if len(value) == 7 and value.endswith("0"):
+            value = value[:-1]
+
+        try:
+            return f"{value[:2]}:{value[2:4]}:{value[4:6]}"
+        except IndexError:
+            return value
+
+    @staticmethod
+    def _parse_expiration_date(date: str, date_format: str = None) -> str:
+        """Try to parse expiration dates, which have several different formats. Returns the last day of expiration month
+        when a custom date format is not provided."""
+        if date_format:
+            try:
+                return datetime.strptime(date, date_format).date().strftime('%Y-%m-%d')
+            except ValueError:
+                pass
+
+        exp = None
+
+        for fmt in ['%m%Y', '%m/%y']:
+            try:
+                exp = datetime.strptime(date, fmt).date()
+                break
+            except ValueError:
+                pass
+
+        if not exp:
+            raise ValueError
+        else:
+            last_day = pendulum.parse(str(exp)).end_of("month")
+            return last_day.strftime('%Y-%m-%d')
---
+++ data_utils/file_interfaces/fixed_width/file_spec.py
@@ -0,0 +1,169 @@
+import csv
+import logging
+import re
+import os
+from collections import defaultdict
+from enum import Enum
+from typing import Any, Dict
+
+from data_utils.connectors.s3_connector import S3Connector
+from data_utils.settings import Settings
+from sqlalchemy import NUMERIC, VARCHAR, DATE, JSON, TIME
+
+logger = logging.getLogger(__name__)
+
+
+class FileFormatEnum(Enum):
+    EBCDIC = 'EBCDIC'
+    ASCII = 'ASCII'
+    LATIN_1 = 'LATIN_1'
+
+
+FORMAT_ENCODING_MAP = {
+    FileFormatEnum.EBCDIC: "cp037",
+    FileFormatEnum.ASCII: "ascii",
+    FileFormatEnum.LATIN_1: "latin-1"
+}
+
+
+class FileSpec:
+    def __init__(self, settings: Settings, file_config: Dict[str, Any]):
+        self.settings = settings
+        self.s3_conn = S3Connector()
+        self.layout_filename = file_config.get("layout_name")
+        self.format = FileFormatEnum(file_config.get("format"))
+        self.encoding = FORMAT_ENCODING_MAP.get(self.format)
+        self.record_length = file_config.get("record_length")
+        self.parse_by_length = file_config.get("parse_by_length")
+        self.record_type_field_pos = (file_config.get("record_type_start_pos"),
+                                      file_config.get("record_type_end_pos"))
+        self.record_type_field_name = file_config.get("record_type_field_name") or "Record Type"
+        self.header_trailer_start_pos = file_config.get("header_trailer_start_pos")
+        self.header_trailer_end_pos = file_config.get("header_trailer_end_pos")
+        self.trailer_start_pos = file_config.get("trailer_start_pos", self.header_trailer_start_pos)
+        self.trailer_end_pos = file_config.get("trailer_end_pos", self.header_trailer_end_pos)
+        self.cast_fields = file_config.get("cast_fields", {})
+        self.sequence_fields = file_config.get("sequence_fields", {})
+        self.nested_field_name = file_config.get("nested_field_name")
+        self.nest_json_fields = file_config.get("nest_json_fields", {})
+        self.layout = defaultdict(list)
+        self.dtypes = {"DETAIL_RECORD_NUMBER": NUMERIC(11, 0)}
+
+        self._generate_layout()
+
+    def _generate_layout(self) -> None:
+        """
+        Reads CSV layout file and builds a dict of tuples, with a key for each "Record Type". E.g.
+        CIF has record types 5 and 6, so two dict keys. Also populates dtypes which are used when
+        loading the fields into databases.
+        """
+        download_path = self.settings.get("file_config", "download_path")
+        file_path = f"{download_path}{self.layout_filename}"
+
+        if self.settings.getboolean("file_config", "use_s3"):
+            bucket = self.settings.get("file_config", "layout_bucket")
+            self.s3_conn.download_file(bucket, self.layout_filename, file_path)
+
+        with open(file_path, 'r') as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                if row.get(self.record_type_field_name):
+                    key = row.get(self.record_type_field_name)
+                else:
+                    key = "000"
+
+                try:
+                    name = row["Copybook Element Name"].strip()
+                    data_type = row["Data Type"].strip()
+                    start = int(row["Start"])
+                    end = int(row["End"])
+                    length = int(row["Length"])
+                    logic = row["Logic Type"].strip()
+                    pic_clause = row["Pic Clause"].strip()
+                except ValueError:
+                    logger.info(f"Error parsing layout row: record type {key}, field_name: {name}")
+                    continue
+
+                # Fields can be cast to different types by defining in yaml file "cast_fields" item.
+                if name in self.cast_fields:
+                    data_type = self.cast_fields[name]["data_type"]
+                    pic_clause = self.cast_fields[name]["pic_clause"]
+
+                # Add tuple to layout for use in parsing
+                self.layout[key].append((name, data_type, start, end, length, logic, pic_clause))
+
+                # Add sqlalchemy type to dtypes for use in loading
+                self._build_dtypes(name, data_type, length, pic_clause)
+
+        # Add dtypes for nested JSON columns
+        self._add_json_dtypes()
+
+        if self.settings.getboolean("file_config", "use_s3"):
+            # Try to clean up downloaded layout after dict is created, but leave test files.
+            if "sample_test_files" not in file_path:
+                try:
+                    os.remove(file_path)
+                except Exception as e:
+                    logger.error(f"Error trying to clean up layout file: {e}")
+
+    def _build_dtypes(self, name: str, data_type: str, length: int, pic_clause: str):
+        """
+        Adds the field's SQLAlchemy type to dtypes dicts. For use in loading with Pandas.
+        """
+        parsed_name = self.parse_field_name(name)
+        if parsed_name != "" and "FILLER" not in parsed_name:
+            if name in self.sequence_fields:
+                for i in range(1, self.sequence_fields[name] + 1):
+                    sequence_name = f"{parsed_name}_{i}"
+                    self.dtypes[sequence_name] = self._sqlalchemy_type(data_type, length, pic_clause)
+            else:
+                self.dtypes[parsed_name] = self._sqlalchemy_type(data_type, length, pic_clause)
+
+    def _add_json_dtypes(self):
+        """
+        Certain files e.g. EVT, AUD are too large for MySQL row size. Fields are compressed
+        into JSON blobs, so we need to provide a dtype for the field and its corresponding
+        "STRUCTURE" field, which tells what dtype each child attr is.
+        """
+        # EVT File is too large to load to SQL. Fields are nested by event type.
+        if self.nested_field_name:
+            self.dtypes[self.nested_field_name.upper()] = JSON
+            self.dtypes[self.nested_field_name.upper() + "_STRUCTURE"] = JSON
+
+        # AUD file is too large, but no common fields to nest like EVT file. Certain prefixes chosen to be nested.
+        if self.nest_json_fields:
+            for k, v in self.nest_json_fields.items():
+                key = v.get("json_field_name")
+                self.dtypes[key.upper()] = JSON
+                self.dtypes[key.upper() + "_STRUCTURE"] = JSON
+
+    @staticmethod
+    def _sqlalchemy_type(data_type: str, length: int, pic_clause: str):
+        """Returns SQLAlchemy column type based on provided data_type, length, and pic_clause."""
+        if data_type == "N" and "." not in pic_clause and "V" not in pic_clause:
+            return NUMERIC(length, 0)
+        elif data_type == "N" and ("." in pic_clause or "V" in pic_clause):
+            parts = re.findall(r'\((.*?) *\)', pic_clause)
+            integer = int(parts[0])
+            decimal = int(parts[1])
+
+            # Set mysql precision to total number of digits stored.
+            integer = integer + decimal
+
+            return NUMERIC(integer, decimal)
+        elif data_type == "D":
+            return DATE()
+        elif data_type == "T":
+            return TIME()
+        else:
+            return VARCHAR(length)
+
+    @staticmethod
+    def parse_field_name(name: str) -> str:
+        """Converts original copybook element name to SQL friendly version using only underscores
+        as a delimiter between words. Also strips the first string before the first underscore. This
+        is usually just the file name abbreviation that we already have from the file itself."""
+        convert_to_underscores = name.upper().replace("-", "_").replace("(", "_").replace(")", "_").strip("_")
+        strip_prefix = ''.join(convert_to_underscores.split("_", 1)[1:]).strip()
+        return strip_prefix
